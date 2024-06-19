ef92943feaad6ac13da09b4d57c2306a2e085447
Jerick Nucasa <nucasa.jerick@gmail.com>
2024-06-19 10:59:20 +0800

    c3

--- controller.py
+++ controller.py
@@ -9,5 +9,38 @@ def index():
     pets = Pet.getall_pets()
     return render_template("index.html", allpets=pets)
 
+@app.route("/pet/new")
+def newpet():
+    return render_template("newpet.html")
+
+@app.route("/process/new/pet", methods=['POST'])
+def create_new_pet():
+    petname = request.form['petname']
+    pettype = request.form['pettype']
+    petdesc = request.form['petdesc']
+    petskill1 = request.form.get('skill1')
+    petskill2 = request.form.get('skill2')
+    petskill3 = request.form.get('skill3')
+    data = {
+        "petname": petname,
+        "pettype": pettype,
+        "petdesc": petdesc,
+        "petskill1": petskill1,
+        "petskill2": petskill2,
+        "petskill3": petskill3
+    }
+    Pet.new_pet(data)
+    return redirect("/")
+
+@app.route("/profile/<id>")
+def profile(id):
+    data = {
+        "id": int(id)
+    }
+    pet = Pet.get_one(data)
+    if not pet:
+        return redirect("/")
+    return render_template("profile.html", pet=pet)
+
 if __name__ == "__main__":
     app.run(debug=True)
--- pets.py
+++ pets.py
@@ -23,3 +23,23 @@ class Pet:
         for pet in results:
             pets.append(cls(pet))  # Create instances of Pet
         return pets
+
+    @classmethod
+    def get_one(cls, data):
+        query = """
+        SELECT id, petname, pettype, petdesc, petskill1, petskill2, petskill3, created_at, updated_at 
+        FROM pets
+        WHERE id = %(id)s
+        """
+        result = connecttoMysql("market_db").query_db(query, data)
+        if result:
+            return cls(result[0])
+        return False
+
+    @classmethod
+    def new_pet(cls, data):
+        query = """
+        INSERT INTO pets (petname, pettype, petdesc, petskill1, petskill2, petskill3) 
+        VALUES (%(petname)s, %(pettype)s, %(petdesc)s, %(petskill1)s, %(petskill2)s, %(petskill3)s)
+        """
+        return connecttoMysql("market_db").query_db(query, data)
